e5fbab9be41ce5ae81e2a1b2c41bbd2ea6148db0
Diogo Sousa Borges <diogosousa2266@gmail.com>
2020-05-26 13:35:41 -0400

    Criação dos Scripts de Banco de Dados

--- Database/scripts/DDL.setup.sql
+++ Database/scripts/DDL.setup.sql
@@ -1,6 +1,5 @@
 IF NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = 'FavoDeMel')
 BEGIN
-	USE [master];
 	CREATE DATABASE [FavoDeMel];
 END
 GO
@@ -8,10 +7,116 @@ GO
 USE [FavoDeMel];
 GO
 
-CREATE TABLE [dbo].[Teste] 
+CREATE TABLE [dbo].[ComandaSituacao] 
 (
 	[Id] INT NOT NULL,
 	[Nome] VARCHAR(180) NOT NULL,
-	CONSTRAINT [PK_Teste] PRIMARY KEY ([Id]) ON [PRIMARY]
+	CONSTRAINT [PK_ComandaSituacao] PRIMARY KEY ([Id]) ON [PRIMARY]
+)
+GO
+
+CREATE TABLE [dbo].[Comanda] 
+(
+	[Id] INT IDENTITY (1,1) NOT NULL,
+	[Numero] INT NOT NULL UNIQUE,
+	[SituacaoId] INT NOT NULL,
+	CONSTRAINT [PK_Comanda] PRIMARY KEY CLUSTERED ([Id]) ON [PRIMARY],
+	CONSTRAINT [FK_Comanda_ComandaSituacao] FOREIGN KEY ([SituacaoId]) REFERENCES [dbo].[ComandaSituacao](Id)
+)
+GO
+
+CREATE TABLE [dbo].[ComandaMovimento] 
+(
+	[Id] INT IDENTITY(1,1) NOT NULL,
+	[DataAbertura] DATETIME NOT NULl,
+	[DataFechamento] DATETIME NULl,
+	[ComandaId] INT NOT NULL,
+	CONSTRAINT [PK_ComandaMovimento] PRIMARY KEY CLUSTERED ([Id]) ON [PRIMARY],
+	CONSTRAINT [FK_ComandaMovimento_Comanda] FOREIGN KEY ([ComandaId]) REFERENCES [dbo].[Comanda](Id)
+)
+GO
+
+CREATE TABLE [dbo].[ProdutoTipo] 
+(
+	[Id] INT NOT NULL,
+	[Nome] VARCHAR(150) NOT NULl,	
+	CONSTRAINT [PK_ProdutoTipo] PRIMARY KEY ([Id]) ON [PRIMARY]
+)
+GO
+
+CREATE TABLE [dbo].[Produto] 
+(
+	[Id] INT IDENTITY(1,1) NOT NULL,
+	[Nome] VARCHAR(150) NOT NULl,	
+	[Observacao] VARCHAR (255) NULL,
+	[TempoPreparo] INT NULL,
+	[Valor]	DECIMAL(19,2) NOT NULL,
+	[TipoId] INT NOT NULL
+	CONSTRAINT [PK_Produto] PRIMARY KEY ([Id]) ON [PRIMARY],
+	CONSTRAINT [FK_Produto_ProdutoTipo] FOREIGN KEY ([TipoId]) REFERENCES [dbo].[Produto]([Id])
+)
+GO
+
+CREATE TABLE [dbo].[PedidoSituacao] 
+(
+	[Id] INT NOT NULL,
+	[Nome] VARCHAR(150) NOT NULl,	
+	CONSTRAINT [PK_PedidoSituacao] PRIMARY KEY ([Id]) ON [PRIMARY]
+)
+GO
+
+CREATE TABLE [dbo].[Pedido] 
+(
+	[Id] INT IDENTITY(1,1) NOT NULL,
+	[Observacao] VARCHAR (255) NULL,
+	[Data] DATETIME NOT NULL,
+	[ComandaMovimentoId] INT NOT NULL,
+	[SituacaoId] INT NOT NULL,
+	CONSTRAINT [PK_Pedido] PRIMARY KEY ([Id]) ON [PRIMARY],
+	CONSTRAINT [FK_Pedido_ComandaMovimento] FOREIGN KEY ([ComandaMovimentoId]) REFERENCES [dbo].[ComandaMovimento]([Id]),
+	CONSTRAINT [FK_Pedido_PedidoSituacao] FOREIGN KEY ([SituacaoId]) REFERENCES [dbo].[PedidoSituacao]([Id])
+)
+GO
+
+CREATE TABLE [dbo].[PedidoItemSituacao] 
+(
+	[Id] INT NOT NULL,
+	[Nome] VARCHAR(150) NOT NULl,	
+	CONSTRAINT [PK_PedidoItemSituacao] PRIMARY KEY ([Id]) ON [PRIMARY]
+)
+GO
+
+CREATE TABLE [dbo].[PedidoItem] 
+(
+	[Id] INT IDENTITY(1,1) NOT NULL,
+	[Observacao] VARCHAR (255) NULL,
+	[PedidoId] INT NOT NULL,
+	[ProdutoId] INT NOT NULL,
+	[SituacaoId] INT NOT NULL,
+	CONSTRAINT [PK_PedidoItem] PRIMARY KEY ([Id]) ON [PRIMARY],
+	CONSTRAINT [FK_PedidoItem_Pedido] FOREIGN KEY ([PedidoId]) REFERENCES [dbo].[Pedido]([Id]),
+	CONSTRAINT [FK_PedidoItem_Produto] FOREIGN KEY ([ProdutoId]) REFERENCES [dbo].[Produto]([Id]),
+	CONSTRAINT [FK_PedidoItem_PedidoItemSituacao] FOREIGN KEY ([SituacaoId]) REFERENCES [dbo].[PedidoItemSituacao]([Id])
+)
+GO
+
+CREATE TABLE [dbo].[PedidoItemProducaoPrioridade] 
+(
+	[Id] INT NOT NULL,
+	[Nome] VARCHAR(150) NOT NULl,	
+	CONSTRAINT [PK_PedidoItemProducaoPrioridade] PRIMARY KEY ([Id]) ON [PRIMARY]
+)
+GO
+
+CREATE TABLE [dbo].[PedidoItemProducao] 
+(
+	[Id] INT IDENTITY(1,1) NOT NULL,
+	[DataInicio] DATETIME NULL,
+	[DataFim] DATETIME NULL,
+	[PedidoItemId] INT NOT NULl,	
+	[PrioridadeId] INT NOT NULL,
+	CONSTRAINT [PK_PedidoItemProducao] PRIMARY KEY ([Id]) ON [PRIMARY],
+	CONSTRAINT [FK_PedidoItemProducao_PedidoItem] FOREIGN KEY ([PedidoItemId]) REFERENCES [dbo].[PedidoItem]([Id]),
+	CONSTRAINT [FK_PedidoItemProducao_PedidoItemProducaoPrioridade] FOREIGN KEY ([PrioridadeId]) REFERENCES dbo.[PedidoItemProducaoPrioridade]([Id])
 )
 GO
--- Database/scripts/DML.setup.sql
+++ Database/scripts/DML.setup.sql
@@ -1,5 +1,60 @@
 USE [FavoDeMel];
 
-INSERT INTO [dbo].[Teste] ([Id], [Nome]) VALUES
-(1, 'Teste')
+INSERT INTO dbo.[ComandaSituacao] ([Id], [Nome]) VALUES
+(1, 'Aberta'),
+(2, 'Fechada')
+GO
+
+DECLARE @ComandaCount INT = 0;
+WHILE @ComandaCount < 50
+BEGIN
+    SET @ComandaCount = @ComandaCount + 1;
+
+	INSERT INTO [dbo].[Comanda] ([Numero], [SituacaoId]) VALUES
+	( @ComandaCount, 2 )    
+END
+GO
+
+INSERT INTO [dbo].[PedidoSituacao] ([Id], [Nome]) VALUES
+( 1, 'Criado'),
+( 2, 'Entregue')
+GO
+
+INSERT INTO [dbo].[PedidoItemSituacao] ([Id], [Nome]) VALUES
+( 1, 'Aguardando Produção'),
+( 2, 'Em Produção'),
+( 3, 'Finalizado'),
+( 4, 'Entregue'),
+( 5, 'Cancelado')
+GO
+
+INSERT INTO [dbo].[PedidoItemProducaoPrioridade] ([Id], [Nome]) VALUES
+( 1, 'Normal'),
+( 2, 'Alta'),
+( 3, 'Urgente')
+GO
+
+INSERT INTO [dbo].[ProdutoTipo] ([Id], [Nome]) VALUES
+( 1, 'Carne'),
+( 2, 'PratoExecutivo'),
+( 3, 'Massa'),
+( 4, 'Saladas'),
+( 5, 'Cerveja'),
+( 6, 'Drink'),
+( 7, 'Suco'),
+( 8, 'Shake'),
+( 9, 'Refrigerante')
+GO
+
+INSERT INTO [dbo].[Produto]([Nome], [Observacao], [TempoPreparo],[Valor], [TipoId]) VALUES
+('Picanha Na Chapa', null, 20, 39.90 , 1),
+('Parmegiana de Filé Bovino', 'Acompanha Arroz e BataFrita', 30, 35.90 , 2),
+('Espaguete à carbonara', null, 30, 25.90 , 3),
+('Salada Caesar', null, 10, 10 , 4),
+('Brahma 600ml', null, null, 10.50 , 5),
+('Cozumel Brahma', null, 5, 15 , 6),
+('Suco de Uva 1l', null, 5, 15 , 7),
+('Milkshake de Chocolate 300ml', null, 8, 10 , 8),
+('Coca KS', null, null, 2.75 , 9),
+('Fanta KS', null, null, 2.50 , 9)
 GO
